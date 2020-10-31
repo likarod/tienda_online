0973b490e93de2aaadb211bb61e7f546a8fafdf5
Likaro-nav <63605973+Likaro-nav@users.noreply.github.com>
2020-10-31 21:11:34 +0100

    31/10 21:11h - Creación de las carpetas de los componentes

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import {Dropdown} from 'react-bootstrap';
+
+import Menu from '../Menu/Menu'
+import './Nav.css';
+
+
+ 
+class Nav extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+  
+
+  render() {
+    return (
+      <div className="App-header">
+        <div className="header_menu_drch">
+            <ul className="Lista_nav">
+              <li><Menu/></li>
+            </ul>
+          </div>
+          <div className="header_menu_izq">
+            <ul className="Lista_nav">
+              <Dropdown>
+                <div className="div_items">
+                  <li>
+                    <Link to="/resumen" className="link">
+                      <box-icon name='cart-alt' size="sm"></box-icon>
+                    </Link>
+                  </li> 
+                </div>
+              </Dropdown>
+            </ul>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default Nav;
